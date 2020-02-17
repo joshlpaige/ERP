e44c64713b319eb25910ed5b63baf5aa5649fba3
Josh Paige <paigejl@yaho.com>
2020-02-17 13:24:42 -0500

    compreport

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userName = "";
+  password = "";
+  registeredUsers = [];
+
+  loginError = false;
+  loginSuccess = false;
+
+  constructor(private data : DataService) { 
+    this.registeredUsers = data.getUsers();
+  }
 
   ngOnInit() {
   }
 
+  validateLogin(){
+    // travel the array
+    // compare user and password with each element in the array
+    // if you find a match, then login is successful
+    for(var i = 0; i < this.registeredUsers.length; i++){
+      var user = this.registeredUsers[i];
+      if(user.userName == this.userName && user.password == this.password){
+        console.log("Credentials are correct, you are logged in!");
+        this.loginSuccess = true;
+        return true;
+      }
+    }
+
+    console.log("wrong credentials");
+    this.loginError = true;
+
+  }
+
+  
 }
--- src/app/pages/user-register/user-register.component.ts
+++ src/app/pages/user-register/user-register.component.ts
@@ -10,16 +10,42 @@ import { DataService } from 'src/app/services/data.service';
 export class UserRegisterComponent implements OnInit {
 
   model: User = new User();
-  reTypePassword = "";  
+  reTypePassword: string;
+  savedAlertVisible = false;
 
-  constructor(private dataSrv : DataService) { }
+  constructor(private dataSrv: DataService) { }
 
   ngOnInit() {
   }
 
-  save(){
+  save() {
     console.log("saving user", this.model, this.reTypePassword);
 
     this.dataSrv.saveUser(this.model);
+
+    this.model = new User();
+    this.reTypePassword = null;
+    this.savedAlertVisible = true;
+  }
+
+  isPasswordStrong() {
+    var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    return strongRegex.test(this.model.password);
+  }
+
+  isDataCorrect() {
+
+
+    if (this.isPasswordStrong()) {
+      return true;
+    }
+
+    return !this.model.email
+      || !this.model.userName
+      || !this.model.password
+      || this.model.password.length < 6
+      || this.model.password != this.reTypePassword
+
   }
 }
+
